92901aa087548479df9b39facbec4cae8cf97cf2
Daniel Patrón Gómez <dpatrongomez@gmail.com>
2018-07-19 07:44:59 +0200

    Add Close Verification

--- app/src/main/java/es/dpatrongomez/papas/MainActivity.java
+++ app/src/main/java/es/dpatrongomez/papas/MainActivity.java
@@ -1,11 +1,14 @@
 package es.dpatrongomez.papas;
 
+import android.app.AlertDialog;
 import android.app.DownloadManager;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ForwardingListener;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -45,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         papas.getSettings().setCacheMode(WebSettings.LOAD_DEFAULT);
         papas.getSettings().setDomStorageEnabled(true);
         papas.getSettings().setAllowFileAccess(true);
-        papas.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
         papas.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
         papas.setWebViewClient(new WebViewClient());
         papas.setWebChromeClient(new WebChromeClient() {
@@ -61,8 +63,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
         });
-        papas.setDownloadListener(new DownloadListener()
-        {
+        papas.setDownloadListener(new DownloadListener() {
 
             @Override
 
@@ -73,38 +74,25 @@ public class MainActivity extends AppCompatActivity {
 
                 DownloadManager.Request request = new DownloadManager.Request(
                         Uri.parse(url));
-
-
                 request.setMimeType(mimeType);
-
-
                 String cookies = CookieManager.getInstance().getCookie(url);
-
-
                 request.addRequestHeader("cookie", cookies);
-
-
                 request.addRequestHeader("User-Agent", userAgent);
-
-
-                request.setDescription("Downloading file...");
-
-
+                request.setDescription(getString(R.string.descargando));
                 request.setTitle(URLUtil.guessFileName(url, contentDisposition,
                         mimeType));
-
-
                 request.allowScanningByMediaScanner();
-
-
                 request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
                 request.setDestinationInExternalFilesDir(MainActivity.this,
-                        Environment.DIRECTORY_DOWNLOADS,".pdf");
+                        Environment.DIRECTORY_DOWNLOADS, ".pdf");
                 DownloadManager dm = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
-                dm.enqueue(request);
-                Toast.makeText(getApplicationContext(), "Descargando archivo",
+                if (dm != null) {
+                    dm.enqueue(request);
+                }
+                Toast.makeText(getApplicationContext(), R.string.descargando,
                         Toast.LENGTH_LONG).show();
-            }});
+            }
+        });
 
     }
 
@@ -140,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         if (papas.canGoForward()) {
             papas.goForward();
         } else {
-            Toast.makeText(this, "No puedes ir hacia delante", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, R.string.erroravanzar, Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -149,7 +137,24 @@ public class MainActivity extends AppCompatActivity {
         if (papas.canGoBack()) {
             papas.goBack();
         } else {
-            finish();
+            final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("¿Quieres salir de la aplicación");
+            builder.setCancelable(true);
+            builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+
         }
     }
 }

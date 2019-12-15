e82ac7357f4849ae624563b704af0ecef7be5021
dpatrongomez <dpatrongomez@gmail.com>
2019-12-15 11:09:33 +0100

    Fix NullPointerException

--- app/src/main/java/es/dpatrongomez/papas/Login.java
+++ app/src/main/java/es/dpatrongomez/papas/Login.java
@@ -1,25 +1,16 @@
 package es.dpatrongomez.papas;
 
-import android.app.ProgressDialog;
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import es.dpatrongomez.papas.R;
+import java.util.Objects;
 
 public class Login extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getSupportActionBar().hide();
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_login);
 
 
